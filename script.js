57018b5fd6593a48e1e9b8678a997df4701d8293
Maria Carrera <meriscarrera@gmail.com>
2024-02-23 15:24:00 +0100

    First char toUpperCase + color by pokemon type added + search bar added

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const body = document.body;
 const main = document.createElement('main');
 body.appendChild(main);
+
+//Creation de la search Box
+const searchBox = document.createElement('div');
+const searchBoxInput = document.createElement('input');
+searchBoxInput.setAttribute('type', 'text');
+searchBoxInput.setAttribute('placeholder', 'Pokemon Name');
+searchBoxInput.classList.add('pokemonName');
+const searchBoxBtn = document.createElement('button');
+searchBoxBtn.innerText = '🔍';
+searchBoxBtn.setAttribute('id', 'search');
+main.appendChild(searchBox);
+searchBox.appendChild(searchBoxInput);
+searchBox.appendChild(searchBoxBtn);
+
+//Creation du conteneur de pokemons
 const content = document.createElement('div');
 content.setAttribute('id', 'content');
 main.appendChild(content);
@@ -10,13 +25,22 @@ function getPokemon(id) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      let types = data.types
+        .map(
+          (type) => `<p class="type ${type.type.name}">${type.type.name}</p>`
+        )
+        .join('');
+
+      const name = data.name;
+      const nameUpperCase = name[0].toUpperCase() + name.slice(1);
+
       content.innerHTML += `<div class="pokemonBox">
                     <img src=${data.sprites.other['official-artwork'].front_default} alt=${data.name} />
                     <div class="pokemonInfo">
-                    <h1>${data.name}</h1>
-                    <p>Weigth: ${data.weight}</p>
+                    <h1>${nameUpperCase}</h1>
+                    <p>Weight: ${data.weight}</p>
                     <p>Height: ${data.height}</p>
-                    <p>Type: ${data.types[0].type.name}</p>
+                    ${types}
                     </div>`;
     })
     .catch((err) => console.log('imposible de contacter le serveur', err));
